06c86603985b7072aa6a9e5fb7d5e8f79ed76a3a
otienosteve <steveotieno701@gmail.com>
2022-05-26 14:19:49 +0300

    added home

--- silverback/urls.py
+++ silverback/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 from . import views
 urlpatterns = [
+    path('',views.home),
     path('admin/', admin.site.urls),
     path('home/',views.home),
     path('other/',include('real.urls'))
