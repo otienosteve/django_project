130eb326f0beecb4d4324c9fb5220a53e6bf9fe0
otienosteve <steveotieno701@gmail.com>
2022-05-26 13:38:49 +0300

    try solve error

--- silverback/settings.py
+++ silverback/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*y2ut*)p2199#bw7v)hw0n@k4&i#d_rym-!+tq#1)^q3ll)+dk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
